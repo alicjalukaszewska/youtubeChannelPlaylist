3cbb19453cb26e99a1c1a736823b8eff1076950a
Alicja <alicja.x.lukaszewska@gmail.com>
2018-02-07 15:58:57 +0100

    progress

--- code.js
+++ code.js
@@ -2,6 +2,7 @@
 const channelId = "UCppPagXEXp7Ls6XM2Pk1vGQ";
 const videoHeight = 300;
 const videoWidth = 500;
+const video = $('#video');
 
 $(document).ready(function(){
 	$.get(
@@ -29,11 +30,33 @@ $(document).ready(function(){
 			function (data) {
 				$.each(data.items, function(i, item){
 					const videoTitle = item.snippet.title;
-					const videoId = item.snippet.resourceId.videoId;
-					const output = `<li><iframe src="https://www.youtube.com/embed/${videoId}" height="${videoHeight}" width="${videoWidth}"></iframe></li>`;
-					$('#results').append(output);
+					const list = `<li><img data-id="${item.snippet.position}" class="thumbnail" src="${item.snippet.thumbnails.default.url}"></li>`;
+					$('#results').append(list);
+					$('#title').html(videoTitle);
+					if (video.data('id') == `${item.snippet.position}`) getCurrentVideo(item);
 				})
 			}
 		);
 	}
-})
+
+	function getCurrentVideo (item) {
+		const videoId = item.snippet.resourceId.videoId;
+		video.html(`<img data-id="${videoId}" class="thumbnail" src="${item.snippet.thumbnails.high.url}">`);
+	}
+
+    function runVideo() {
+        var iframe = document.createElement("iframe");
+        var embed = "https://www.youtube.com/embed/ID?autoplay=1";
+        iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+        iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("allowfullscreen", "1");
+        this.parentNode.replaceChild(iframe, this);
+    }
+
+	video.click(runVideo);
+	$( "#results").on( "click", "img", function( event ) {
+		console.log(event.target);
+		let newId = $(this).data('id');
+    	video.data('id', newId);
+	})
+});
