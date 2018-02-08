398666619d3ee3cb59bc609c195da842bc1c4c6f
Alicja <alicja.x.lukaszewska@gmail.com>
2018-02-08 16:24:34 +0100

    Page tokens

--- code.js
+++ code.js
@@ -10,6 +10,7 @@ const maxResults = 5;
 const video = $('#video');
 let playlistId;
 let flag = false;
+let nextPage;
 
 $(document).ready(function(){
 	$.get(
@@ -19,6 +20,8 @@ $(document).ready(function(){
 		key: 'AIzaSyBXpLUT6WzX24CqJb4rM4PKpWh7lfC3pZY'}, 
 
 		function (data) {
+
+
 			$.each(data.items, function(i, item){
 				playlistId = item.contentDetails.relatedPlaylists.uploads;
 				getVideos(playlistId);
@@ -26,7 +29,7 @@ $(document).ready(function(){
 		}
 	);
 
-	function getVideos (playlistId) {
+	function getVideos (playlistId, pageToken) {
 		$.get(
 			"https://www.googleapis.com/youtube/v3/playlistItems", {
 			part: 'snippet',
@@ -34,26 +37,59 @@ $(document).ready(function(){
 			maxResults,
 			key: 'AIzaSyBXpLUT6WzX24CqJb4rM4PKpWh7lfC3pZY'}, 
 
-			function (data) {
+			function (data) {	
+				if (pageToken) {
+				   data.pageToken = pageToken;
+				}
 				console.log(data);
+				nextPage = data.nextPageToken;
+				prevPage = data.prevPageToken;
 				$.each(data.items, function(i, item){
+					video.attr("data-id", data.items[0].snippet.resourceId.videoId);
 					if (!flag) {
-						const list = `<li><img data-id="${item.snippet.position}" class="thumbnail" src="${item.snippet.thumbnails.default.url}"></li>`;
+						const list = `<li><img data-id="${item.snippet.resourceId.videoId}" class="thumbnail" src="${item.snippet.thumbnails.default.url}"></li>`;
 						$('#results').append(list);
 					}
-					if (video.attr('data-id') == `${item.snippet.position}`) getCurrentVideo(item);
+					
+					searchVideo(item);
 				})
 				flag = true;
 			}
 		);
 	}
 
+	function getNextPage (playlistId, pageToken) {
+		$.get(
+			"https://www.googleapis.com/youtube/v3/playlistItems", {
+			part: 'snippet',
+			pageToken,
+			playlistId,
+			maxResults,
+			key: 'AIzaSyBXpLUT6WzX24CqJb4rM4PKpWh7lfC3pZY'}, 
+
+			function (data) {	
+				nextPage = data.nextPageToken;
+				prevPage = data.prevPageToken;
+				$.each(data.items, function(i, item){
+					const list = `<li><img data-id="${item.snippet.resourceId.videoId}" class="thumbnail" src="${item.snippet.thumbnails.default.url}"></li>`;
+					$('#results').append(list);
+					searchVideo(item);
+				})
+			}
+		);
+	}
+
+	function searchVideo (item) {
+		if (video.attr('data-id') == `${item.snippet.resourceId.videoId}`) getCurrentVideo(item); 
+	}
+
+
 	function getCurrentVideo (item) {
 		const videoId = item.snippet.resourceId.videoId;
 		const videoTitle = item.snippet.title;
 		$('#title').html(videoTitle);
 		runVideo(videoId);
-		if (!flag) video.html(`<img data-id="${videoId}" class="main-thumbnail" src="${item.snippet.thumbnails.maxres.url}">`);
+		if (!flag) video.html(`<img data-id="${item.snippet.resourceId.videoId}" class="main-thumbnail" src="${item.snippet.thumbnails.maxres.url}">`);
 		
 	}
 
@@ -69,10 +105,23 @@ $(document).ready(function(){
 		video.html(iframe);
     }
 
-	
+    function nextPage(e) {
+    	e.preventDefault();
+	  	getNextPage(playlistId, nextPage);
+	}
+
+    function prevPage(e) {
+    	e.preventDefault();
+	  	getNextPage(playlistId, prevPage);
+	}
+
+	$('#next').click(nextPage);
+	$('#prev').click(prevPage);
+
 	$( "#results").on( "click", "img", function() {
 		let newId = $(this).data('id');
     	video.attr("data-id", newId);
+    	console.log(newId);
     	getVideos(playlistId);
 	})
 
