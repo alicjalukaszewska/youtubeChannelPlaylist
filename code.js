3e0a1b04362705d0c9092f7440ce21d8fbc5cdb3
Alicja <alicja.x.lukaszewska@gmail.com>
2018-02-08 14:00:14 +0100

    Load on click

--- code.js
+++ code.js
@@ -1,8 +1,15 @@
 
-const channelId = "UCppPagXEXp7Ls6XM2Pk1vGQ";
-const videoHeight = 300;
+
+/* If you don't know channel ID, use this site: http://johnnythetank.github.io/youtube-channel-name-converter/ */
+const channelId = "UChAHYPBvyaQIpjyTSdQhOMQ";
+const videoHeight = 280;
 const videoWidth = 500;
+const maxResults = 5;
+
+
 const video = $('#video');
+let playlistId;
+let flag = false;
 
 $(document).ready(function(){
 	$.get(
@@ -13,7 +20,7 @@ $(document).ready(function(){
 
 		function (data) {
 			$.each(data.items, function(i, item){
-				const playlistId = item.contentDetails.relatedPlaylists.uploads;
+				playlistId = item.contentDetails.relatedPlaylists.uploads;
 				getVideos(playlistId);
 			})
 		}
@@ -24,39 +31,50 @@ $(document).ready(function(){
 			"https://www.googleapis.com/youtube/v3/playlistItems", {
 			part: 'snippet',
 			playlistId,
-			maxResults: 25,
+			maxResults,
 			key: 'AIzaSyBXpLUT6WzX24CqJb4rM4PKpWh7lfC3pZY'}, 
 
 			function (data) {
+				console.log(data);
 				$.each(data.items, function(i, item){
-					const videoTitle = item.snippet.title;
-					const list = `<li><img data-id="${item.snippet.position}" class="thumbnail" src="${item.snippet.thumbnails.default.url}"></li>`;
-					$('#results').append(list);
-					$('#title').html(videoTitle);
-					if (video.data('id') == `${item.snippet.position}`) getCurrentVideo(item);
+					if (!flag) {
+						const list = `<li><img data-id="${item.snippet.position}" class="thumbnail" src="${item.snippet.thumbnails.default.url}"></li>`;
+						$('#results').append(list);
+					}
+					if (video.attr('data-id') == `${item.snippet.position}`) getCurrentVideo(item);
 				})
+				flag = true;
 			}
 		);
 	}
 
 	function getCurrentVideo (item) {
 		const videoId = item.snippet.resourceId.videoId;
-		video.html(`<img data-id="${videoId}" class="thumbnail" src="${item.snippet.thumbnails.high.url}">`);
+		const videoTitle = item.snippet.title;
+		$('#title').html(videoTitle);
+		runVideo(videoId);
+		if (!flag) video.html(`<img data-id="${videoId}" class="main-thumbnail" src="${item.snippet.thumbnails.maxres.url}">`);
+		
 	}
 
-    function runVideo() {
-        var iframe = document.createElement("iframe");
-        var embed = "https://www.youtube.com/embed/ID?autoplay=1";
-        iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
-        iframe.setAttribute("frameborder", "0");
-        iframe.setAttribute("allowfullscreen", "1");
-        this.parentNode.replaceChild(iframe, this);
+    function runVideo(videoId) {
+		const iframe = document.createElement("iframe");
+		var embed = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+		iframe.setAttribute("src", embed);
+		iframe.setAttribute("width", `${videoWidth}`);
+		iframe.setAttribute("height", `${videoHeight}`);
+		iframe.setAttribute("frameborder", "0");
+		iframe.setAttribute("controls", "2");
+		iframe.setAttribute("allowfullscreen", "1");
+		video.html(iframe);
     }
 
-	video.click(runVideo);
-	$( "#results").on( "click", "img", function( event ) {
-		console.log(event.target);
+	
+	$( "#results").on( "click", "img", function() {
 		let newId = $(this).data('id');
-    	video.data('id', newId);
+    	video.attr("data-id", newId);
+    	getVideos(playlistId);
 	})
+
+	video.click(() => getVideos(playlistId));
 });
